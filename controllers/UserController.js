4f080d30cff422320fb98e694e6f376cb6cd6c31
AceHunterr <aggarwalmehul26@gmail.com>
2023-10-03 15:57:16 +0530

    setup tested

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,4 +11,13 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json({ users });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { createUser, getAllUsers };
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { createUser } = require("../controllers/UserController");
+const { createUser, getAllUsers } = require("../controllers/UserController");
 
 router.post("/create", createUser);
+router.get("/all", getAllUsers);
 
 module.exports = router;
