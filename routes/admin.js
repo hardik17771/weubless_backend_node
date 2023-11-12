3aac4cbb23fb4c0d27ddde66afa080dd72aa3efc
AceHunterr <aggarwalmehul26@gmail.com>
2023-11-12 23:47:33 +0530

    Added update Profile in admin panel

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,18 @@ router.get("/", (req, res) => {
   res.render("admin/index", { title: "Admin Dashboard" });
 });
 
+// Route to handle user profile update
+router.post("/update-profile", async (req, res) => {
+  const data = req.body; // Assuming the data is sent via POST request body
+  const result = await apiController.updateProfile(data);
+
+  if (result.code === 200) {
+    res
+      .status(200)
+      .json({ message: "Profile updated successfully", data: result.data });
+  } else {
+    res.status(result.code).json({ message: "Failed to update profile" });
+  }
+});
+
 module.exports = router;
